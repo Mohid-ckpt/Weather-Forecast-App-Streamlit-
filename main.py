ffbebf4a3131addaa1c8124fcb1b452373a88f69
Mohid <hussain.mohid@gmail.com>
2024-11-11 17:15:55 +0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Imports:
+
+import streamlit as st
+
+# Frontend:
+
+st.title("Weather Forecast for the Next Days")
+place = st.text_input("Place: ")
+days = st.slider("Forecast Days", min_value=1, max_value=5,
+                 help="Select the number of days forecasted")
+option = st.selectbox("Select the data to view",("Temperature","Sky"))
+st.subheader(f"{option} for the next {days} days in {place}:")
