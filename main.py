9f337d8e2053518ba64e6b4e752f9314d9bd26ff
Mohid <hussain.mohid@gmail.com>
2024-11-14 20:47:48 +0500

    Added functionality in CLI.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Imports:
 
 import streamlit as st
+import plotly.express as px
+from backend import get_data
 
 # Frontend:
 
@@ -10,3 +12,10 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of days forecasted")
 option = st.selectbox("Select the data to view",("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}:")
+
+
+
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", 'y': 'Temperature (C)'})
+st.plotly_chart(figure)
