415da68518c5697021768bc47ee4dd2a2b77902a
Mohid <hussain.mohid@gmail.com>
2024-11-16 16:00:34 +0500

    Alpha Release

--- backend.py
+++ backend.py
@@ -2,20 +2,13 @@ import requests
 
 API_KEY = '6d7e19fa4c71fcb05fa50aab48c57e8c'
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data['list']
     filtered_data = filtered_data[:8*forecast_days]
-    try:
-        if kind == 'Temperature' or 'temperature':
-            filtered_data = [dict['main']['temp'] for dict in filtered_data]
-        if kind == 'Sky':
-            filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
-    except:
-        print("Please enter a valid type of data.")
     return filtered_data
 
 if __name__=="__main__":
-    print(get_data(place='Tokyo', forecast_days=3,kind='Temperature'))
+    print(get_data(place='Tokyo', forecast_days=3))
--- main.py
+++ main.py
@@ -5,17 +5,36 @@ import plotly.express as px
 from backend import get_data
 
 # Frontend:
-
+st.set_page_config(layout="wide")
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
-days = st.slider("Forecast Days", min_value=1, max_value=5,
+days = st.slider("Forecast Days", min_value=1, max_value=15,
                  help="Select the number of days forecasted")
 option = st.selectbox("Select the data to view",("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}:")
 
+if place:
+    try:
+        # Get the required data:
+        filtered_data = get_data(place,days)
 
+        if option.lower() == 'temperature':
+            # Filter the Data:
+            temperatures = [dict['main']['temp'] for dict in filtered_data]
+            temperatures = [temperature/10 for temperature in temperatures]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create the temperature plot:
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", 'y': 'Temperature (C)'})
+            st.plotly_chart(figure)
 
-d, t = get_data(days)
-
-figure = px.line(x=d, y=t, labels={"x": "Date", 'y': 'Temperature (C)'})
-st.plotly_chart(figure)
+        elif option == 'Sky':
+            # Filter the data:
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            images = {'Clear':'images/clear.png','Clouds':'images/cloud.png',
+                      'Rain':'images/rain.png','Snow':'images/snow.png'}
+            image_list = [images[condition] for condition in sky_conditions]
+            # Viewing the sky data:
+            st.image(image_list, width=115)
+    except:
+        st.write("Please enter a valid place")
